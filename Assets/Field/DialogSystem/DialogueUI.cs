8e6d6b37ce4b457c1b743754991f9b7f4fb796a8
ViscLu <112127908+ViscLu@users.noreply.github.com>
2026-03-26 15:17:11 -0400

    Add character Icon on Dialog UI

--- Assets/Field/DialogSystem/DialogueUI.cs
+++ Assets/Field/DialogSystem/DialogueUI.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class DialogueUI : MonoBehaviour
 {
@@ -9,6 +10,12 @@ public class DialogueUI : MonoBehaviour
     [Header("Text")]
     [SerializeField] private TMP_Text speakerNameText;
     [SerializeField] private TMP_Text dialogueLineText;
+
+    [Header("Character Icon")]
+    [SerializeField] private Sprite HugoIcon;
+    [SerializeField] private Sprite TenetIcon;
+    [SerializeField] private Sprite ResidentIcon;
+    [SerializeField] private Image speakerIcon;
 
     private void Awake()
     {
@@ -21,10 +28,40 @@ public class DialogueUI : MonoBehaviour
             dialogueRoot.SetActive(true);
 
         if (speakerNameText != null)
+        {
             speakerNameText.text = speakerName;
+            Debug.Log(speakerName);
+            switch (speakerName)
+            {
+                case "Hugo":
+                    if (HugoIcon != null)
+                        speakerIcon.color = Color.white;
+                    speakerIcon.sprite = HugoIcon;
+                    break;
+                case "Tenet":
+                    if (TenetIcon != null)
+                        speakerIcon.color = Color.white;
+                    speakerIcon.sprite = TenetIcon;
+                    break;
+                case "Resident":
+                    if (ResidentIcon != null)
+                        speakerIcon.color = Color.white;
+                    speakerIcon.sprite = ResidentIcon;
+                    break;
+                default:
+                    speakerIcon.sprite = null;
+                    break;
+            }
 
-        if (dialogueLineText != null)
-            dialogueLineText.text = line;
+            if (speakerName == "Tenet")
+            {
+                Debug.Log("True");
+                speakerIcon.sprite = TenetIcon;
+            }
+
+            if (dialogueLineText != null)
+                dialogueLineText.text = line;
+        }
 
     }
 
